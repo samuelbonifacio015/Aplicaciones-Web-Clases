2324b08f1808068dfdc8935ca2225e0f2ac0542a
Samuel Bonifacio <131225632+samuelbonifacio015@users.noreply.github.com>
2025-09-10 19:44:29 -0500

    updated hello-asp-net-developer

---
+++ Semana 3/Miercoles 10-09/hello-asp-net-developer/hello-asp-net-developer/hello-asp-net-developer/Generic/Domain/Model/Entities/Developer.cs
@@ -0,0 +1,52 @@
+namespace hello_asp_net_developer.Generic.Domain.Model.Entities;
+
+/// <summary>
+/// Represents a developer with a unique identifier, first name, and last name
+/// and trimmed first and last names.
+/// </summary>
+
+public class Developer
+{
+    
+    /// <summary>
+    /// Gets the unique identifier for the developer.
+    /// </summary>
+    /// 
+    public Guid Id { get; } = Guid.NewGuid();
+    
+    /// <summary>
+    /// Gets the developer FirstName.
+    /// </summary>
+    public string FirstName { get; }
+    
+    /// <summary>
+    /// Gets the developer LastName.
+    /// </summary>
+    public string LastName { get; }
+    
+    /// <summary>
+    /// Initializes a new instance of the Developer class with the privded first and last names.
+    /// </summary>
+    /// <param name="firstName">The developer's frist name, may be null or contain whitespace</param>
+    /// <param name="lastName">The developer's last name may be null or contain whitespace</param>
+
+    public Developer(string firstName, string lastName)
+    {
+        FirstName = string.IsNullOrWhiteSpace(firstName) ? "" : firstName.Trim();
+        LastName = string.IsNullOrWhiteSpace(lastName) ? "" : lastName.Trim();
+    }
+    
+    /// <summary>
+    /// Returns the full name of the developer concatenating first and last names
+    /// </summary>
+    /// <returns>The full name as a trimmed string.</returns>
+
+    public string GetFullName() => $"{FirstName} {LastName}".Trim();
+    
+    /// <summary>
+    /// Checks if either the first name or last name is empty or consists only of whitespace.
+    /// </summary>
+    /// <returns>True if any name is empty, false otherwise</returns>
+    
+    public bool IsAnyNameEmpty() => string.IsNullOrWhiteSpace(FirstName) || string.IsNullOrWhiteSpace(LastName);
+}
